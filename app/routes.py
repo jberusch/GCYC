424420593339305b7157ab941d1998207ff9357a
Joseph George Berusch <jberusch@dewgong.cs.uchicago.edu>
2018-11-17 17:32:18 -0600

    ready for openpyxl

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import SearchForm
 
 @app.route('/')
@@ -11,11 +11,14 @@ def index():
 def search():
 	form = SearchForm()
 	if form.validate_on_submit():
-		flash('Student ID: {}, filter1: {}'.format(form.student_id.data, form.filter1.data))
-		return redirect(url_for('search_results'))
+		form_data = {
+			"student_id": form.student_id.data,
+			"filter1": form.filter1.data
+		}
+		return render_template('search.html', title = 'Search', form = form, results = form_data)
 
 	return render_template('search.html', title = 'Search', form = form)
 
-@app.route('/search_results')
+@app.route('/search_results', methods=['GET','POST'])
 def search_results():
 	return "search results go here"
